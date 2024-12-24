52cae82e8d8e26de6caa2da2d3ddb8fa3798453e
Srushti Sunkad <srushtisunkad.cs23@bmsce.ac.in>
2024-12-25 00:06:33 +0530

    Lab7_LinkedList Operations.c to Lab7_LinkedList Operation And Stacj queue s.c

--- Lab7_LinkedList Operation And Stacj queue s.c
+++ Lab7_LinkedList Operation And Stacj queue s.c
@@ -1,3 +1,4 @@
+1)
 #include<stdio.h>
 #include<stdlib.h>
 #include<stdbool.h>
@@ -287,3 +288,318 @@ Enter your choice: 7
 
 Process returned 0 (0x0)   execution time : 79.779 s
 Press any key to continue.
+
+
+    
+2.WAP to Implement Singly Linked List to stimulate stack and queue operations.
+ 
+I)
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+struct Node{
+    int data;
+    struct Node *next;
+};
+
+void push(int x,struct Node* *top){
+    struct Node newnode=(struct Node)malloc(sizeof(struct Node));
+    newnode->data=x;
+    newnode->next=*top;
+    *top=newnode;
+}
+
+void pop(struct Node* *top){
+    if(*top==NULL){
+        printf("Empty\n");
+    }
+    struct Node *temp=*top;
+    *top=(*top)->next;
+    free(temp);
+}
+void peek(struct Node* top) {
+    if (top == NULL) {
+        printf("Empty\n");
+    } else {
+        printf("%d \n", top->data);
+    }
+}
+
+void display(struct Node* top) {
+    struct Node* temp = top;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;  
+    }
+    printf("\n");
+}
+
+void main(){
+    int data,choice;
+    struct Node *top=NULL;
+
+    while(true){
+        printf("1.Push\n");
+        printf("2.Pop\n");
+        printf("3.Peek\n");
+        printf("4.Display\n");
+        printf("5.Exit\n");
+
+        printf("Enter your choice: ");
+        scanf("%d",&choice);
+
+        switch(choice){
+            case 1:printf("Enter data:");
+                   scanf("%d",&data);
+                   push(data,&top);
+                   break;
+            case 2:pop(&top);
+                   break;
+            case 3:peek(top);
+                   break;
+            case 4:display(top);
+                   break;
+            case 5:exit(0);
+                   break;
+            default:printf("Invalid Choice\n");
+                    break;
+        }
+    }
+}
+
+//OUTPUT
+1.Push
+2.Pop
+3.Peek
+4.Display
+5.Exit
+Enter your choice: 1
+Enter data:12
+1.Push
+2.Pop
+3.Peek
+4.Display
+5.Exit
+Enter your choice: 1
+Enter data:45
+1.Push
+2.Pop
+3.Peek
+4.Display
+5.Exit
+Enter your choice: 1
+Enter data:78
+1.Push
+2.Pop
+3.Peek
+4.Display
+5.Exit
+Enter your choice: 1
+Enter data:89
+1.Push
+2.Pop
+3.Peek
+4.Display
+5.Exit
+Enter your choice: 4
+89 78 45 12 
+1.Push
+2.Pop
+3.Peek
+4.Display
+5.Exit
+Enter your choice: 3
+89 
+1.Push
+2.Pop
+3.Peek
+4.Display
+5.Exit
+Enter your choice: 2
+1.Push
+2.Pop
+3.Peek
+4.Display
+5.Exit
+Enter your choice: 2
+1.Push
+2.Pop
+3.Peek
+4.Display
+5.Exit
+Enter your choice: 4
+45 12 
+1.Push
+2.Pop
+3.Peek
+4.Display
+5.Exit
+Enter your choice: 3
+45 
+1.Push
+2.Pop
+3.Peek
+4.Display
+5.Exit
+Enter your choice: 5
+Process returned 0 (0x0)   execution time : 79.779 s
+Press any key to continue.
+
+    
+II)
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node{
+    int data;
+    struct Node *next;
+};
+
+void enqueue(struct Node* front,struct Node *rear,int data){
+    struct Node newnode=(struct Node)malloc(sizeof(struct Node));
+    newnode->data=data;
+    newnode->next=NULL;
+    
+    if(*front==NULL && *rear==NULL){
+        *front=*rear=newnode;
+    }
+    else{
+        (*rear)->next=newnode;
+        *rear=newnode;
+    }
+}
+
+void dequeue(struct Node* front,struct Node *rear){
+    if(*front==NULL && *rear==NULL){
+        printf("Queue is empty\n");
+    }
+    else if(*front==*rear){
+        *front=*rear=NULL;
+    }
+    else{
+        *front=(*front)->next;
+    }
+}
+
+void display(struct Node* front){
+    struct Node *temp=front;
+    if(front==NULL){
+        printf("Queue is empty\n");
+    }
+    while(temp!=NULL){
+        printf("%d ",temp->data);
+        temp=temp->next;
+    }
+    printf("\n");
+}
+
+void main(){
+    int data,choice;
+    struct Node *front=NULL;
+    struct Node *rear=NULL;
+    
+    while(1){
+        printf("1.Enqueue\n");
+        printf("2.Dequeue\n");
+        printf("3.Display\n");
+        printf("4.Exit\n");
+        
+        printf("Enter your choice:");
+        scanf("%d",&choice);
+        
+        switch(choice){
+            case 1:printf("Enter data:");
+                   scanf("%d",&data);
+                   enqueue(&front,&rear,data);
+                   break;
+            case 2:dequeue(&front,&rear);
+                   break;
+            case 3:display(front);
+                   break;
+            case 4:exit(0);
+                   break;
+            default:printf("Invalid choice\n");
+        }
+    }
+}
+
+//OUTPUT
+
+1.Enqueue
+2.Dequeue
+3.Display
+4.Exit
+Enter your choice:1
+Enter data:11
+1.Enqueue
+2.Dequeue
+3.Display
+4.Exit
+Enter your choice:1
+Enter data:22
+1.Enqueue
+2.Dequeue
+3.Display
+4.Exit
+Enter your choice:1
+Enter data:33
+1.Enqueue
+2.Dequeue
+3.Display
+4.Exit
+Enter your choice:1
+Enter data:44
+1.Enqueue
+2.Dequeue
+3.Display
+4.Exit
+Enter your choice:3
+11 22 33 44 
+1.Enqueue
+2.Dequeue
+3.Display
+4.Exit
+Enter your choice:2
+1.Enqueue
+2.Dequeue
+3.Display
+4.Exit
+Enter your choice:2
+1.Enqueue
+2.Dequeue
+3.Display
+4.Exit
+Enter your choice:3
+33 44 
+1.Enqueue
+2.Dequeue
+3.Display
+4.Exit
+Enter your choice:2
+1.Enqueue
+2.Dequeue
+3.Display
+4.Exit
+Enter your choice:2
+1.Enqueue
+2.Dequeue
+3.Display
+4.Exit
+Enter your choice:3
+Queue is empty
+
+1.Enqueue
+2.Dequeue
+3.Display
+4.Exit
+Enter your choice:2
+Queue is empty
+1.Enqueue
+2.Dequeue
+3.Display
+4.Exit
+Enter your choice:4
+
+
+=== Code Execution Successful ===
